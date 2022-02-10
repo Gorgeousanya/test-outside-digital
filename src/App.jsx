bb319b7783101b1abd786f0bf6ef66a98fdc2f3d
Анна Плохотнюк <annaplokhotnyuk@MacBook-Pro-Anna-2.local>
2022-02-10 23:53:21 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -18,17 +18,21 @@ export class App extends React.Component {
       rest: 0,
       count: 0,
       payments: [],
+      error: false,
     }
     this.Home = this.Home.bind(this);
+    this.Tax = this.Tax.bind(this);
+    this.Calculate = this.Calculate.bind(this);
 }
 
 Tax(){
-  if (Number(this.state.salary)!==0){
+  if ((Number(this.state.salary)!==0)&&(!isNaN(Number(this.state.salary)))){
+    this.state.error=false;
   //this.setState({tax: Number(this.state.salary)*1.56});
   this.state.tax=Number(this.state.salary)*1.56;
   let credit = 260000;
   let i=0;
-  //console.log(this.state.tax, Number(this.state.salary));
+  console.log(this.state.tax, Number(this.state.salary));
   while (credit>0){
     credit-=this.state.tax;
     i++;
@@ -38,7 +42,11 @@ Tax(){
   this.state.count= i;
   //console.log(this.state.rest, i);
 }
-  else console.log("error");
+  else {
+    this.state.error=true;
+    console.log(this.state.error, "error");
+
+}
 }
 
 Calculate(){
@@ -54,14 +62,20 @@ Calculate(){
 }
 
 Home(){
+  const tax="Налоговый вычет";
+  const useTax="Используйте налоговый вычет чтобы погасить ипотеку досрочно. Размер налогового вычета составляет не более 13% от своего официального годового дохода.";
+  const salary="Ваша зарплата в месяц";
+  const sum="Итого можете внести в качестве досрочных";
+  const less="Что уменьшаем?";
+  const payment="Платёж";
+  const term="Срок";
+  const add="Добавить"
   return (
     <div className="App">
       <header className="App-header">
         
-        <button className="App-Button" onClick={()=>this.setState({modalIsOpen: !this.state.modalIsOpen})}>
-        
-          Налоговый вычет
-        
+        <button className="App-Button" onClick={()=>this.setState({modalIsOpen: !this.state.modalIsOpen})}>       
+          {tax}
         </button>
         
         <Modal
@@ -73,72 +87,55 @@ Home(){
         ariaHideApp={false}
       >
         <button onClick={()=>this.setState({modalIsOpen: !this.state.modalIsOpen})} style={{position: "relative", top: "15px", left:"92%", borderColor: "rgba(0, 0, 0, 0)", backgroundColor: 'rgba(0, 0, 0, 0)'}}><img src={close} /></button>
-        <h3 className="H3">Налоговый вычет</h3>
-        <h5 className="H4"> Используйте налоговый вычет чтобы погасить ипотеку досрочно. Размер налогового вычета составляет не более 13% от своего официального годового дохода.</h5>
+        <h3 className="H3">{tax}</h3>
+        <h5 className="H4"> {useTax}</h5>
         
-        <div style={{position: "relative", left: "32px", right: "0", top: "0px", bottom: "66.67%", fontFamily: "Lab Grotesque",
-fontWeight: "500",
-fontSize: "14px",
-lineHeight: "24px",
-color: "#000000",}}>Ваша зарплата в месяц</div>
+        <div style={{position: "relative", left: "32px", right: "0", top: "0px", bottom: "66.67%", fontFamily: "Lab Grotesque", fontSize: "14px", lineHeight: "24px", color: "#000000",}}>
+          {salary}
+        </div>
         
-          <input className="App-Input" value={this.state.salary} onChange={(event) => {
+        <input className="App-Input" placeholder='Введите данные' autoFocus={false} value={this.state.salary} style={!this.state.error ? {paddingLeft: "8px"} : {border: "1px solid #DC3131", paddingLeft: "8px"}} onChange={(event) => {
         this.setState({salary:event.target.value})
       }}/>
           <button className="App-Calculate" onClick={()=>{this.setState({calculate: true}); console.log(Number(this.state.salary)); this.Tax(); this.Calculate(); this.state.payments.forEach((item)=> console.log(item))}}>Рассчитать</button>
         {
-          this.state.calculate ? (
-
+          this.state.calculate&&!this.state.error ? (
           <div>
-          <h3 style={{position: "relative", left: "32px", right: "0", top: "16px", bottom: "66.67%", fontFamily: "Lab Grotesque",
-          fontWeight: "500",
-          fontSize: "14px",
-          lineHeight: "24px",
-          color: "#000000", }}>Итого можете внести в качестве досрочных:</h3>
-          
-          
-          <ListGroup variant="flush" style={{position: "relative", left: "32px", top:"10px", width: "34vmax", fontFamily: "Lab Grotesque",
-          fontWeight: "500",
-          fontSize: "14px",
-          lineHeight: "24px", padding: "0"}}>
-            { this.state.payments.map((item)=>(
+          <h3 className='App-Sum'>{sum}:</h3>         
+          <ListGroup variant="flush" className='App-List'>
+            { this.state.payments.map((item)=>
+            (
               <div>
-            <ListGroup.Item style={{padding: "0"}}>
-            <Checkbox
-  size="small"
-  defaultChecked
-  sx={{
-    color: "red",
-    '&.Mui-checked': {
-      color: "red",
-    },
-  }}
-/>{item} </ListGroup.Item>
-</div>))
-          
-}
-        </ListGroup>
-        </div>
-          ) : (<div></div>)
+              <ListGroup.Item  style={{ borderTopColor: "rgba(0, 0, 0, 0)", borderLeftColor: "rgba(0, 0, 0, 0)", borderRightColor: "rgba(0, 0, 0, 0)", padding: "0"}}>
+              <Checkbox
+              size="small"
+              defaultChecked
+              sx={{
+                color: "#DFE3E6",
+                '&.Mui-checked': {
+                  color: "red",
+                },
+              }}
+                />{item} </ListGroup.Item>
+                </div>))         
+                }
+            </ListGroup>
+            </div>
+            ) : (<div></div>)
         }
-        <div style={{position: "relative", width: "112px", height:"24px", left: "32px",  top: "24px", fontFamily: "Lab Grotesque",
-fontWeight: "500",
-fontSize: "14px",
-lineHeight: "24px",
-color: "#000000", marginBottom: "2rem"}}>
-        <div >Что уменьшаем?</div>
-        
-          <button className="App-Payment">Платёж</button>
-          <button className="App-Term"> Срок</button>
+        <div className='App-Less'>
+        <div >{less}</div> 
+          <button className="App-Payment">{payment}</button>
+          <button className="App-Term"> {term}</button>
         </div>
-        <button className="App-Add">Добавить</button>
+        <button className="App-Add">{add}</button>
       </Modal>
       </header>
     </div>
   );
 }
   render(){
-    console.log();
+  console.log("render");
   return this.Home();
 }
 }
